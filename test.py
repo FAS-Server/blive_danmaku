ed1f07cf3d0229ccb839230c43916f5ac47b00e8
YehowahLiu <redme@live.cn>
2022-07-30 18:08:45 +0800

    test: simple test

---
+++ test.py
@@ -0,0 +1,29 @@
+from blive_danmaku.room import Room
+from blive_danmaku.config import RoomConfig
+import time
+
+if __name__ == '__main__':
+    conf = {
+        'id': 160651,
+        'nickname': 'Test',
+        'listener': [
+            "DANMU_MSG", "COMBO_SEND", "GUARD_BUY", "SUPER_CHAT_MESSAGE",
+            "PREPARING", "LIVE", "INTERACT_WORD", "VERIFICATION_SUCCESSFUL"
+        ]
+    }
+    room = Room(RoomConfig(**conf))
+
+    print('尝试连接')
+    room.start()
+    while room.stream.get_status() != room.stream.STATUS_ESTABLISHED:
+        pass
+    time.sleep(5)
+    print('尝试断开')
+    room.cancel()
+    room.join()
+    time.sleep(5)
+    print('尝试连接')
+    del room
+    room = Room(RoomConfig(**conf))
+    room.start()
+    room.join()
